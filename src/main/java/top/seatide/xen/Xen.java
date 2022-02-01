9cf239580b017e08e58650265fac2c4d12812cb9
Subilan <suyuanliang1995@gmail.com>
2022-02-01 17:08:07 +0800

    initial

---
+++ src/main/java/top/seatide/xen/Xen.java
@@ -0,0 +1,16 @@
+package top.seatide.xen;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Xen extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
