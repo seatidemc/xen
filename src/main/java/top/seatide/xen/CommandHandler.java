521d9618a39bfcd8ec36304828d6ec716b50aa38
Subilan <suyuanliang1995@gmail.com>
2022-02-05 22:07:10 +0800

    finish tab completer and fix

--- src/main/java/top/seatide/xen/CommandHandler.java
+++ src/main/java/top/seatide/xen/CommandHandler.java
@@ -2,7 +2,10 @@ package top.seatide.xen;
 
 import com.google.common.base.CaseFormat;
 import org.apache.commons.lang.StringUtils;
-import org.bukkit.*;
+import org.bukkit.Bukkit;
+import org.bukkit.GameRule;
+import org.bukkit.World;
+import org.bukkit.WorldType;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
@@ -19,11 +22,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CommandHandler implements TabExecutor {
     public final static String[] ARGS = { "load", "unload", "create", "tp", "list", "rule", "mode", "diff", "difficulty", "help", "reloadPlugin" };
 
-    public List<String> getResult(String arg, List<String> commands, CommandSender sender) {
+    public List<String> getResult(String arg, List<String> commands) {
         List<String> result = new ArrayList<>();
         StringUtil.copyPartialMatches(arg, commands, result);
         Collections.sort(result);
@@ -41,13 +45,45 @@ public class CommandHandler implements TabExecutor {
                         candy.add(m);
                     }
                 }
-                return getResult(args[0], candy, sender);
+                return getResult(args[0], candy);
+            }
+            if (args.length == 2) {
+                if (args[0].equals("load")|| args[0].equals("create") || args[0].equals("unload") || args[0].equals("tp")) {
+                    return Bukkit.getWorlds().stream().map(World::getName).distinct().collect(Collectors.toList());
+                }
+                if (args[0].equals("rule")) return List.of("get", "set");
+            }
+
+            if (args.length == 3) {
+                if (args[0].equals("load") || args[0].equals("create")) {
+                    return List.of("NORMAL", "FLAT", "LARGE_BIOMES", "AMPLIFIED");
+                }
+                if (args[0].equals("rule")) return Bukkit.getWorlds().stream().map(World::getName).distinct().collect(Collectors.toList());
+            }
+
+            if (args.length == 4) {
+                if (args[0].equals("load") || args[0].equals("create")) {
+                    return List.of("NORMAL", "NETHER", "THE_END");
+                }
+                if (args[0].equals("rule")) return Arrays.stream(GameRule.values()).map(GameRule::getName).distinct().collect(Collectors.toList());
+            }
+
+            if (args.length == 5) {
+                if (args[0].equals("rule")) {
+                    var rule = GameRule.getByName(args[3]);
+                    if (rule != null) {
+                        if (rule.getType() == Boolean.class) {
+                            return List.of("true", "false");
+                        }
+                    }
+                }
             }
         }
         return result;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (command.getName().equalsIgnoreCase("xen")) {
             if (args.length == 0) {
@@ -88,7 +124,7 @@ public class CommandHandler implements TabExecutor {
                         LogUtil.send(sender, LogLevel.ERROR, label.equals("load") ? "加载" : "创建" + "失败，无法获取世界。");
                         return true;
                     }
-                    Worlds.save(name, type.name(), env.name());
+                    Worlds.saveAsRecord(name, type.name(), env.name());
                     LogUtil.send(sender, LogLevel.SUCCESS, "成功" + (label.equals("load") ? "加载" : "创建") + "世界 &a" + name + "&r。");
                     break;
                 }
@@ -110,6 +146,7 @@ public class CommandHandler implements TabExecutor {
                         LogUtil.send(sender, LogLevel.SUCCESS, "成功卸载世界 &a" + name + "&r。");
                     } else {
                         LogUtil.send(sender, LogLevel.ERROR, "无法卸载世界 &c" + name + "&r。");
+                        LogUtil.send(sender, LogLevel.ERROR, "可能是由于该世界中仍然有玩家或者其它插件的影响。");
                     }
                     break;
                 }
@@ -129,7 +166,7 @@ public class CommandHandler implements TabExecutor {
 
                     var name = args[1];
                     if (!allowDimension && List.of("DIM1", "DIM-1").contains(name)) {
-                        LogUtil.send(sender, LogLevel.ERROR, "不能进行维度传送。");
+                        LogUtil.send(sender, LogLevel.ERROR, "维度之间的传送已被禁用。");
                         return true;
                     }
                     var world = Bukkit.getWorld(name);
@@ -145,7 +182,7 @@ public class CommandHandler implements TabExecutor {
 
                 /* xen list */
                 case "list": {
-                    var worlds = Worlds.getWorldNameList(allowDimension);
+                    var worlds = Worlds.getLoadedWorldList(allowDimension);
                     LogUtil.send(sender, LogLevel.INFO, "当前可传送世界（共 &a" + worlds.size() + "&r 个）：");
                     LogUtil.send(sender, LogLevel.INFO, "&a" + StringUtils.join(worlds, ", "));
                     break;
@@ -189,7 +226,6 @@ public class CommandHandler implements TabExecutor {
                             }
                             if (target.getType() == Integer.class) {
                                 Integer value = args.length == 5 ? Integer.parseInt(args[4]) : 1;
-                                //noinspection unchecked
                                 if (world.setGameRule((GameRule<Integer>) target, value)) {
                                     LogUtil.send(sender, LogLevel.SUCCESS, "成功将世界 &b" + name + "&r 的规则 &e" + rule + "&r 设为 &a" + value + "&r。");
                                     return true;
@@ -198,7 +234,6 @@ public class CommandHandler implements TabExecutor {
                                 }
                             } else if (target.getType() == Boolean.class) {
                                 Boolean value = args.length != 5 || Boolean.parseBoolean(args[4]);
-                                //noinspection unchecked
                                 if (world.setGameRule((GameRule<Boolean>) target, value)) {
                                     LogUtil.send(sender, LogLevel.SUCCESS, "成功将世界 &b" + name + "&r 的规则 &e" + rule + "&r 设为 &" + (value ? "atrue" : "cfalse") + "&r。");
                                     return true;
